a28222d4bf3a29e73cfbf4277411983abac8874c
Andrea Lacava <thecave003@hotmail.it>
2017-06-28 23:29:45 +0200

    More error handlings + bugfix

--- reader.py
+++ reader.py
@@ -8,10 +8,10 @@ import os
 import threading
 
 print( "Provo ad aprire porta Arduino...")
-#apre la porta seriale ad un baudrate 9600
+# apre la porta seriale ad un baudrate 9600
 try:
     ser = serial.Serial(
-    port='/dev/ttyACM0',
+    port='COM65',
     baudrate=9600,
     parity=serial.PARITY_ODD,
     stopbits=serial.STOPBITS_TWO,
@@ -19,34 +19,46 @@ try:
     )
 except serial.serialutil.SerialException:
     print( "Errore, porta sbagliata? Dispositivo non collegato?")
-    print("Prima di eseguire lo script settare correttamente la porta, la porta di default è /dev/ttyACM0")
+    print("Prima di eseguire lo script settare correttamente la porta, la porta di default è COM65")
     print( "Termino applicazione")
     sys.exit(-1)
 
-#crea file di log dove salva i dati raccolti dall'arduino
-logger = open("logger.csv",'w')
-plt.axis([0, 2000, 0, 2000])
+# crea file di log dove salva i dati raccolti dall'arduino
+print("Creo un nuovo file di log...\n")
+try:
+	logger = open("logger.csv",'w')
+except IOError:
+	print("Errore in creazione nuovo file di log, controlla che il vecchio file non sia aperto con altri programmi")
+	print("Uscita in corso...")
+	sys.exit(-1)
+
+print( "File di log creato correttamente!")
+print( "Nota che il file logger.csv viene sovrascritto ogni volta, se quindi devi salvare i dati rinomina il file vecchio")
+
+plt.axis([0, 8640, 0.2, 1.1])
 plt.ion()
 
-#the big loop
+# the big loop
 while 1:
     try:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        data=ser.readline()
-        logger.write(data)
-        data = data.strip("\n").strip("\r")
-        data = data.split(';')
-        print( "Pressione ambientale: "+ data[0]+"\nUmidità %: "+ data[1]+"\nTemperatura °C: "+ data[2] +"\nPressione interna: "+data[3]+ "\nTempo: "+data[4])
-        y = data[3]
-        x = data[4]
-        plt.scatter(x, y)
-        plt.pause(0.05)
+		data=ser.readline()
+		os.system('cls' if os.name == 'nt' else 'clear')
+		data.strip('\n')
+		logger.write(data)
+		data = data.strip("\n").strip("\r")
+		data = data.split(';')
+		print( "Pressione ambientale: "+ data[0]+"\nUmidita' %: "+ data[1]+"\nTemperatura (gradi centigradi): "+ data[2] +"\nPressione interna: "+data[3]+ "\nTime: "+data[4])
+		y = data[3]
+		x = data[4]
+		plt.scatter(x, y)
+		plt.pause(0.05)
 
     except KeyboardInterrupt:
         logger.flush()
         plt.close()
         logger.close()
         ser.close()
+        print("Uscita in corso...")
         sys.exit()
 
     except IndexError:
